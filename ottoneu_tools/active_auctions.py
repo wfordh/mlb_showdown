2342a44903985452611f5878563650dabfa520a1
Ford Higgins <wfordh@users.noreply.github.com>
2021-02-02 22:00:27 -0800

    is_mlb and positions

--- ottoneu_tools/active_auctions.py
+++ ottoneu_tools/active_auctions.py
@@ -4,43 +4,99 @@
 # using pybaseball, scrape statcast info
 # v2: scrape RoS steamer / zips projections
 # format data into email and send
+import re
 import requests
 from bs4 import BeautifulSoup
-from pybaseball import statcast_batter, playerid_lookup, statcast_pitcher, cache
+from pybaseball import statcast_batter_exitvelo_barrels, playerid_lookup, statcast_pitcher, cache
+from tqdm import tqdm
 
-league_id = "953"  # put this in env
-base_url = "https://ottoneu.fangraphs.com"
-auction_url = f"{base_url}/{league_id}/transactions"
-resp = requests.get(auction_url)
-soup = BeautifulSoup(resp.content, "html.parser")
-table = soup.find("table")
-thead = [th.get_text() for th in table.find("thead").find_all("th")]
-trows = [
-    [td.get_text().strip() for td in tr.find_all("td")]
-    for tr in table.find("tbody").find_all("tr")
-]
-# want row and link in one list? if so, prob don't want to use list comps
+def main():
+    league_id = "953"  # put this in env
+    base_url = "https://ottoneu.fangraphs.com"
+    current_year = 2021
+    auction_url = f"{base_url}/{league_id}/transactions"
+    auction_url = "https://ottoneu.fangraphs.com/953/transactions?filters%5B%5D=cut&filters%5B%5D=increase"
+    resp = requests.get(auction_url)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    table = soup.find("table")
+    thead = [th.get_text() for th in table.find("thead").find_all("th")]
 
-# filter for correct transaction type and only current transactions
-#  get player name and take link to player page
-# then use lookup to get player data
-# append this link to the main url?
-print([a["href"] for a in trows[0].find_all("a") if "playercard" in a["href"]])
+    auction_players = list()
+    for tr in tqdm(table.find("tbody").find_all("tr")):
+        player_data = [td.get_text().strip() for td in tr.find_all("td")]
+        player_page_url = [a["href"] for a in tr.find_all("a") if "playercard" in a["href"]].pop()
+        player_dict = dict(zip(thead, player_data))
+        if player_dict["Transaction Type"] != "add":
+            continue
+        player_dict["ottoneu_id"] = player_page_url.rsplit("=")[1]
+        player_salary_dict = get_ottoneu_avg_salary(player_dict["ottoneu_id"], league_id)
+        player_dict.update(player_salary_dict)
+        # clean and separate name
+        player_name = clean_name(player_dict["Player Name"])
+        first_name, last_name = player_name.split(maxsplit=1)
+        # could maybe make this smarter by checking the ottoneu player page for minor league level
+        try:
+            id_lookup = playerid_lookup(last_name, first_name)
+            player_dict["mlbam_id"] = id_lookup.key_mlbam.values[0]
+        except IndexError:
+            # player in minors...note somehow?
+            pass
+
+        auction_players.append(player_dict)
+
+    # somehow get all of the player positions
+    # if any(player_positions["is_hitter"]):
+    #     exit_velo_data = statcast_batter_exitvelo_barrels(current_year)
+    #     # add exit velo data to dict
+    # if any(not player_positions["is_hitter"]):
+    #     # currently pybaseball only has individual pitcher data
+    #     pass
+
+    # want row and link in one list? if so, prob don't want to use list comps
+
+    # filter for correct transaction type and only current transactions
+    #  get player name and take link to player page
+    # then use lookup to get player data
+    # append this link to the main url?
+    print(player_dict)
 
 
-# get names via lookup. use cache.enable() for pybaseball here?
-# get statcast data for individual players or for league and then search for players?
+
+    # get names via lookup. use cache.enable() for pybaseball here?
+    # get statcast data for individual players or for league and then search for players?
 
 # will need to clean names and remove jr, sr, etc
-def get_ottoneu_avg_salary(player_id, lg_id=league_id):
-    player_url = f"{base_url}/{lg_id}/playercard"
-    player_resp = requests.get(player_url, params={"id": player_id})
-    player_soup = BeautifulSoup(player_resp.content, "html.parser")
-    salary_info = (
-        player_soup.find("header", {"class": "page-header"})
-        .find("div", {"class": "page-header__secondary"})
-        .find_all("div")[1]
+def get_ottoneu_avg_salary(player_id, lg_id):
+    player_page_dict = dict()
+    url = f"https://ottoneu.fangraphs.com/{lg_id}/playercard"
+    r = requests.get(url, params = {"id":player_id})
+    soup = BeautifulSoup(r.content, 'html.parser')
+    header_data = soup.find('main').find('header', {"class":"page-header"})
+    level_data = header_data.find("div", {"class":"page-header__section--split"}).find("span", {"class":"strong tinytext"}).get_text()
+    player_page_dict["is_mlb"] = False if "(" in level_data else True
+    salary_data = header_data.find("div", {"class":"page-header__secondary"})
+    player_page_dict["positions"] = salary_data.find("div", {"class":"page-header__section--split"}).find("p").get_text().strip().rsplit(maxsplit=1)[1]
+    salary_tags = [tag.get_text() for tag in salary_data.find_all("em")]
+    salary_nums = [num.get_text() for num in salary_data.find_all("a")]
+   
+    for idx, (tag, num) in enumerate(zip(salary_tags, salary_nums)):
+        if idx % 2 == 0:
+            scoring_type = tag
+            player_page_dict[tag] = num
+        else:
+            player_page_dict[f"{tag} - {scoring_type}"] = num
+    return player_page_dict
+
+def clean_name(player_name):
+    name_suffixes = ("jr", "sr", "ii", "iii", "iv", "v")
+    player_name = re.sub(r"([.'])", "", player_name.lower())
+    player_name = (
+        player_name.rsplit(maxsplit=1)[0]
+        if player_name.endswith(name_suffixes)
+        else player_name
     )
-    # all avg, all last 10, scoring specific all, scoring specific last 10
-    # do find_all('em') to get those
-    salary_info.find_all("a")
+    return player_name
+
+
+if __name__ == '__main__':
+    main()
